26ff622aa1c3bfac82cfae8748ddf3b3f00ac3db
Ethan Kiang <zyklotomic@users.noreply.github.com>
2017-02-20 12:04:37 +0000

    Add files via upload
    xrandr monitor connection script

---
+++ connect.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Connecting Monitor on HDMI-2"
+xrandr --output eDP-1 --auto --output HDMI-2 --auto --right-of eDP-1 
+feh --bg-fill /home/ethan/Pictures/Wallpapers/Monstercat_025.jpg
+
+
