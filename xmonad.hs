81bc0ccc26d4259adf026a7d13e44448cf3cdd01
Ethan Kiang <chocopuff298@gmail.com>
2020-08-01 12:52:26 +0800

    xmonad changes

--- xmonad.hs
+++ xmonad.hs
@@ -8,17 +8,19 @@ import XMonad.Layout.NoBorders
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.Tabbed
 import XMonad.Hooks.SetWMName
+import XMonad.Actions.CycleWS
+import XMonad.Actions.GridSelect
 -- import XMonad.Layout.BinarySpacePartition
 
 import qualified Data.Map as M
 
-main = xmonad =<< xmobar (ewmh ethansConfig)
+main = xmobar (ewmh ethansConfig) >>= xmonad
 
 -- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = True
 
-myWorkspaces = ["壹","二","三","四","五","六","七","八","九","十"]
+myWorkspaces = ["壹","二","三","四","五","六","七","八","九"]
 -- ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 myBorderWidth = 2
 myFocusedBorderColor = "#ffa300"
@@ -28,13 +30,23 @@ myTerminal = "urxvtc"
 -- myLayout = smartBorders $ layoutHook def ||| simpleTabbed
 myLayout = smartBorders $ layoutHook def
 
+-- myStartupHook = setWMName "LG3D"
+
 -- keybindings
 myModMask = mod1Mask
 -- super_key = mod4Mask
 localKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-	[ ((modm, xK_space), spawn "rofi -show combi")
+	[ ((modm, xK_space), spawn "rofi -show-icons -show combi")
 	-- For legacy dmenu run, muscle memory
-	, ((modm, xK_p), spawn "rofi -show run")
+	, ((modm, xK_p), spawn "rofi -show-icons -show run")
+	-- Cycle workspaces bindings
+	, ((mod4Mask, xK_Tab), moveTo Next NonEmptyWS)
+	, ((mod4Mask .|. shiftMask, xK_Tab), moveTo Prev NonEmptyWS)
+	-- , ((mod4Mask, xK_Return), shiftTo Next NonEmptyWS >> moveTo Next NonEmptyWS)
+	-- , ((mod4Mask .|. shiftMask, xK_Return), )
+	-- GridSelect
+	, ((modm, xK_g), goToSelected defaultGSConfig)
+	, ((modm .|. shiftMask, xK_g), bringSelected defaultGSConfig)
 	-- Override default for switching layouts
 	, ((modm, xK_BackSpace), sendMessage NextLayout) ]
 
@@ -48,8 +60,8 @@ ethansConfig = def {
 		terminal			= myTerminal,
 		borderWidth			= myBorderWidth, 
  		workspaces			= myWorkspaces,
-		focusedBorderColor  = myFocusedBorderColor,
-		normalBorderColor   = myNormalBorderColor,
+		focusedBorderColor              = myFocusedBorderColor,
+		normalBorderColor               = myNormalBorderColor,
 		keys				= myKeys, 
 		modMask				= myModMask,
 		-- hooks, layouts
