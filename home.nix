1ed94add878d683955fdf0f5ec16e8c02baff3a6
Ethan Kiang <chocopuff298@gmail.com>
2020-08-21 05:47:38 +0800

    migrating to home-manager

---
+++ home.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}:
+
+{
+  home.file = {
+    ".config/ranger/rc.conf".source = ./ranger/rc.conf;
+    ".xmonad/xmonad.hs".source = ./xmonad/xmonad.hs;
+    ".xmobarrc".source = ./xmonad/.xmobarrc;
+    ".config/rofi/config".source = ./rofi/config;
+  };
+}
